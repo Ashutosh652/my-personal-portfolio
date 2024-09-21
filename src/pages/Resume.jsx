435f4ea05bb8a32d087764070df307ce1ecdeae6
Ashutosh Chapagain <ashutoshchapagain0@gmail.com>
2024-09-21 14:47:06 +0545

    Add 'Download PDF' button to resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowAltCircleDown } from "@fortawesome/free-regular-svg-icons/faArrowAltCircleDown";
+
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
 import Main from "../layouts/Main";
 
@@ -24,31 +29,58 @@ const sections = {
   Courses: () => <Courses data={courses} />,
 };
 
-const Resume = () => (
-  <Main
-    title="Resume"
-    description="Ashutosh Chapagain's Resume. Milo Logic and Tech Temple"
-  >
-    <article className="post" id="resume">
-      <header>
-        <div className="title">
-          <h2>
-            <Link to="/resume">Resume</Link>
-          </h2>
-          <div className="link-container">
-            {Object.keys(sections).map((sec) => (
-              <h4 key={sec}>
-                <a href={`#${sec.toLowerCase()}`}>{sec}</a>
-              </h4>
-            ))}
+const Resume = () => {
+  const pdfRef = useRef();
+
+  const downloadPDF = () => {
+    const input = pdfRef.current;
+    html2canvas(input, { scale: 1 }).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF("p", "mm", "A4");
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = pdf.internal.pageSize.getHeight();
+      const imgWidth = canvas.width;
+      const imgHeight = canvas.height;
+      const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
+      const imgX = (pdfWidth - imgWidth * ratio) / 2;
+      const imgY = (pdfHeight - imgHeight * ratio) / 2;
+
+      pdf.addImage(imgData, "PNG", imgX, imgY, imgWidth * ratio, imgHeight * ratio);
+      pdf.save("AshutoshChapagainResume.pdf");
+    });
+  };
+
+  return (
+    <Main
+      title="Resume"
+      description="Ashutosh Chapagain's Resume. Milo Logic and Tech Temple"
+    >
+      <div className="download-button">
+        <button onClick={downloadPDF}>
+          <FontAwesomeIcon icon={faArrowAltCircleDown} /> Download PDF
+        </button>
+      </div>
+      <article className="post" id="resume" ref={pdfRef}>
+        <header>
+          <div className="title">
+            <h2>
+              <Link to="/resume">Resume</Link>
+            </h2>
+            <div className="link-container">
+              {Object.keys(sections).map((sec) => (
+                <h4 key={sec}>
+                  <a href={`#${sec.toLowerCase()}`}>{sec}</a>
+                </h4>
+              ))}
+            </div>
           </div>
-        </div>
-      </header>
-      {Object.entries(sections).map(([name, Section]) => (
-        <Section key={name} />
-      ))}
-    </article>
-  </Main>
-);
+        </header>
+        {Object.entries(sections).map(([name, Section]) => (
+          <Section key={name} />
+        ))}
+      </article>
+    </Main>
+  );
+};
 
 export default Resume;
